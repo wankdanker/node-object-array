635102d2db523e2801a53c43acb93aa085a39040
Dan VerWeire <dverweire@gmail.com>
2016-01-27 22:34:14 -0500

    1.0.0

---
+++ index.js
@@ -0,0 +1,85 @@
+"use strict";
+
+var uuid = require('node-uuid');
+var keys = Object.keys;
+
+module.exports = ObjectArray;
+module.exports.applyPrototype = applyPrototype;
+
+function applyPrototype (target) {
+	target = target || Object;
+	
+	Object.keys(ObjectArray.prototype).forEach(function (key) {
+		target.prototype[key] = ObjectArray.prototype[key];
+	});
+	
+	ObjectArray.call(target);
+}
+
+function ObjectArray () {
+	if (this instanceof ObjectArray === false) {
+		return new ObjectArray();
+	}
+}
+
+ObjectArray.prototype.push = function () {
+	for (var x = 0; x < arguments.length; x++) {
+		this[id()] = arguments[x];
+	}
+};
+
+ObjectArray.prototype.pop = function () {
+	var ks = keys(this);
+	var k = ks[ks.length -1];
+	var o = this[k];
+	
+	delete this[k];
+
+	return o;
+};
+
+ObjectArray.prototype.shift = function () {
+	var ks = keys(this);
+	var k = ks[0];
+	var o = this[k];
+	
+	delete this[k];
+
+	return o;
+};
+
+ObjectArray.prototype.map = function (fn) {
+	var self = this;
+	var ks = keys(self);
+	var result = [];
+	
+	ks.forEach(function (k) {
+		result.push(fn(self[k], k, self)); 
+	});
+	
+	return result;
+};
+
+ObjectArray.prototype.filter = function (fn) {
+	var self = this;
+	var ks = keys(self);
+	var result = [];
+	
+	ks.forEach(function (k) {
+		var o = self[k];
+		
+		if (fn(o, k, self)) {
+			result.push(o);
+		}
+	});
+	
+	return result;
+};
+
+ObjectArray.prototype.length = function () {
+	return keys(this).length;
+};
+
+function id() {
+	return uuid().replace(/-/gi,'');
+}
---
+++ test.js
@@ -0,0 +1,26 @@
+var test = require('tape');
+
+require('./').applyPrototype();
+
+test('usage', function (t) {
+	var a = {};
+
+	a.push({ a : 'Hello' }, { b : 'World' }, { c : 'Once' });
+	
+	t.equal(a.length(), 3);
+	
+	t.deepEqual(a.map(function (o) { return o; }), [{ a : 'Hello' }, { b : 'World' }, { c : 'Once' }] );
+	
+	a.push('hello');
+	a.push('there');
+	
+	t.deepEqual(a.filter(function (o) { return (typeof o === 'string') }), ['hello', 'there'])
+	
+	a.pop();
+	a.pop();
+	
+	t.deepEqual(a.pop(), { c : 'Once' });
+	t.deepEqual(a.shift(), { a : 'Hello' });
+	
+	t.end();
+});
